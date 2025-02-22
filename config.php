db6bbb9974b195d227dd672172afcd65e7706100
esang222 <carinagajessa@gmail.com>
2025-02-23 00:21:08 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
-
-$servername = "localhost";
+$profileImage = 'img/admin.png';
+$adminName = 'Admin01';
+$servername = "127.0.0.1";
 $username = "root";
 $password = "";
 $database = "hospital_soap_system";
-
+$port = 3307;
 
 // increase memory limit
-ini_set('memory_limit', '1024M');//nag error kulang memory for php script
+ini_set('memory_limit', '1024M');
 
 // enable MySQLi error reporting
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
